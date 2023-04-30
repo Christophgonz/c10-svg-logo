3f5c4634ac53cd476373753cb2df461433c0d9e2
cgonz <christoph.gonz@gmail.com>
2023-04-30 03:25:50 -0700

    implemented user prompts and logic, started maker

--- index.js
+++ index.js
@@ -2,3 +2,39 @@ const inquirer = require("inquirer");
 const join = require("path");
 const writeFile = require("fs");
 const maker = require("./lib/maker");
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Enter three characters for your logo",
+    },
+    {
+      type: "input",
+      name: "titleColor",
+      message:
+        "Enter the text color (Enter a hexidecimal code or color keyword)",
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "Choose from the following shapes",
+      choices: ["Circle", "Square", "Triangle"],
+    },
+    {
+      type: "input",
+      name: "shapeColor",
+      message:
+        "Enter the shape's color (Enter a hexidecimal code or color keyword)",
+    },
+  ])
+  .then((res) => {
+    const { title, titleColor, shape, shapeColor } = res;
+    const svg = new Maker(title, titleColor, shape, shapeColor);
+    return writeFile(
+      join(__dirname, ".", "examples", "logo.svg"),
+      svg.create()
+    );
+  })
+  .then(() => console.log("Generated logo.svg"));
